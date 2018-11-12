73b862a4ff741158f086e243e27d451225f9f809
Luis Cruz <luismirandacruz@gmail.com>
2018-11-12 14:07:36 +1100

    find future commits that have refactored

--- sszz/sszz.py
+++ sszz/sszz.py
@@ -18,22 +18,61 @@ def tool(repo, commit, log):
     if log:
         log_numeric_level = getattr(logging, log.upper())
         logging.basicConfig(level=log_numeric_level)
+    click.echo(find_refactoring_commit(repo, commit))
 
-    find_fixing_bug(repo, commit)
-
-
-def find_fixing_bug(repo_dir, commit_sha):
+def find_refactoring_commit(repo_dir, commit_sha):
     """Look for a bug fix that blames change induced by this commit."""
-    logging.info(f'Looking for bug fixing commits for commit {commit_sha} in repo "{repo_dir}".')    
-    logging.info(check_refactoring(repo_dir, commit_sha, commit_sha+"~1"))
+    logging.info(
+        'Looking for bug fixing commits for '
+        f'commit {commit_sha} '
+        f'in repo "{repo_dir}".'
+    )
+    return next(
+        (future_commit
+         for future_commit in get_all_commits_since(repo_dir, commit_sha)
+         if check_refactoring_has_happened(repo_dir, commit_sha, future_commit)),  
+        None
+    )
 
-def get_parent_commit(commit):
-    logging.debug(f"Getting Parent Commit for {commit}.")
-    if len(commit.parents) > 0:
-        return commit.parents[0]
-    raise CommitWithoutParent
+def find_refactoring_commit_binary(repo_dir, commit_sha, future_commits=None):
+    if future_commits is None:
+        future_commits = get_all_commits_since(repo_dir, commit_sha)
+    if len(future_commits) == 0:
+        return None
+    future_commit_index = int((len(future_commits)-1)/2)
+    future_commit = future_commits[future_commit_index]
+    if check_refactoring_has_happened(repo_dir, commit_sha, future_commit):
+        if len(future_commits) == 1:
+            return future_commits[0]
+        else:
+            future_commits = future_commits[:future_commit_index+1]
+    else:
+        future_commits = future_commits[future_commit_index+1:]
+    return find_refactoring_commit_binary(
+        repo_dir,
+        commit_sha,
+        future_commits
+    )
+def check_refactoring_has_happened(repo_dir, commit_a, commit_b):
+    """Check whether, at the moment of commit_b, code from commit_a has been refactored."""
+    logging.info(f"Checking whether commit {commit_b} refactors {commit_a}.")
+    commit_ap = f"{commit_a}^1"
+    commit_bp = f"{commit_b}^1"
+    changes_ap_to_a = git_compare_commits(repo_dir, commit_ap, commit_a)
+    # changes_ap_to_bp = git_compare_commits(repo_dir, commit_ap, commit_bp)
+    changes_ap_to_b = git_compare_commits(repo_dir, commit_ap, commit_b)
+    # changes_a_to_bp = git_compare_commits(repo_dir, commit_a, commit_bp)
+    changes_a_to_b = git_compare_commits(repo_dir, commit_a, commit_b)
+    code_was_refactored_between_a_and_b = ((changes_ap_to_a+changes_a_to_b) != changes_ap_to_b)
+    # code_was_refactored_between_a_and_bp = ((changes_ap_to_a+changes_a_to_bp) != changes_ap_to_bp)
+    code_was_refactored_by_b = (
+        code_was_refactored_between_a_and_b
+        # and
+        # not(code_was_refactored_between_a_and_bp)
+    )
+    return code_was_refactored_by_b
 
-def check_refactoring(repo_dir, commit_a, commit_b):
+def check_refactoring_commit(repo_dir, commit_a, commit_b):
     """Check whether commit_b refactors code from commit_a."""
     logging.info(f"Checking whether commit {commit_b} refactors {commit_a}.")
     commit_ap = f"{commit_a}^1"
@@ -51,6 +90,7 @@ def check_refactoring(repo_dir, commit_a, commit_b):
         not(code_was_refactored_between_a_and_bp)
     )
     return code_was_refactored_by_b
+
 
     
     
@@ -85,6 +125,16 @@ def _get_numeric_var_from_regex_match(pattern, string):
     return 0
 def _add_tuples(t1, t2):
     return tuple(sum(values) for values in zip(t1, t2))
+
+def get_all_commits_since(repo_dir, commit_sha):
+    """Get a list of all commits made after the given commit."""
+    #git log --pretty='%H' 10edd33598c80b6e9453bd8c0117b29f004837d3..HEAD
+    git_caller = git.cmd.Git(repo_dir)
+    output = git_caller.execute(
+        ['git', 'log', "--pretty=%H", f"{commit_sha}..HEAD"]
+    )
+    return output.split('\n')[::-1]
+
 
 class SSZZException(Exception):
     """Base Class for SSZZ exceptions."""
